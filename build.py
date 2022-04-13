f16b573117543ea633c4084bed2b5876840a1b06
Defelo <elodef42@gmail.com>
2022-04-13 12:28:15 +0200

    Improved build

--- build.py
+++ build.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 
 from pathlib import Path
-import yaml
-import shutil
+import mkdocs_gen_files
 import re
 
 CATEGORIES = [
@@ -13,57 +12,29 @@ CATEGORIES = [
     "general",
 ]
 
-with open("mkdocs.yml") as file:
-    config = yaml.load(file, yaml.Loader)
-
-def link_to(src, dst):
-    if hasattr(src, "link_to"):
-        src.link_to(dst)
-    else:
-        dst.write_bytes(src.read_bytes())
-
-for item in config["nav"]:
-    if "Cogs" in item:
-        cog_nav = item["Cogs"]
-        break
-else:
-    print("::error::Could not find cogs in mkdocs navigation!")
-    exit(1)
-
-docs = Path(__file__).parent.joinpath("cogs/pubsub.md")
-if not docs.is_file():
-    print(f"::error::Could not find pubsub documentation")
-    exit(1)
-
-target = Path("pubsub.md")
-link_to(docs, Path("docs").joinpath(target))
-
-cog_nav.clear()
-
-for category in map(Path(__file__).parent.joinpath("cogs").joinpath, CATEGORIES):
-    if not category.is_dir():
-        print(f"::warning::Could not find category {category.name}")
-        continue
-
-    category_nav = []
-    cog_nav.append({category.name.capitalize(): category_nav})
-
-    for cog in sorted(category.iterdir()):
-        if not cog.is_dir():
+with mkdocs_gen_files.open("cogs/SUMMARY.md", "w") as summary:
+    for category in map(Path(__file__).parent.joinpath("cogs").joinpath, CATEGORIES):
+        if not category.is_dir():
+            print(f"::warning::Could not find category {category.name}")
             continue
 
-        docs = cog.joinpath("documentation.md")
-        if not docs.is_file():
-            print(f"::warning::Could not find documentation for {category.name}/{cog.name}")
-            continue
+        print(f"- {category.name.capitalize()}", file=summary)
 
-        with docs.open() as file:
-            name = re.match(r"^#? *(.*)$", file.readline()).group(1).strip()
+        for cog in sorted(category.iterdir()):
+            if not cog.is_dir():
+                continue
 
-        target = Path(f"cogs/{category.name}/{cog.name}.md")
-        Path("docs").joinpath(target).parent.mkdir(parents=True, exist_ok=True)
-        link_to(docs, Path("docs").joinpath(target))
-        category_nav.append({name: str(target)})
+            docs = cog.joinpath("documentation.md")
+            if not docs.is_file():
+                print(f"::warning::Could not find documentation for {category.name}/{cog.name}")
+                continue
 
-with open("mkdocs.yml", "w") as file:
-    yaml.dump(config, file)
+            with docs.open() as src, mkdocs_gen_files.open(f"cogs/{category.name}/{cog.name}.md", "w") as dst:
+                content = src.read()
+                dst.write(content)
+                name = re.match(r"^#? *(.*)$", content.splitlines()[0])[1].strip()
+
+            print(f"    - [{name}]({category.name}/{cog.name}.md)", file=summary)
+
+    with open("cogs/pubsub.md") as src, mkdocs_gen_files.open("pubsub.md", "w") as dst:
+        dst.write(src.read())

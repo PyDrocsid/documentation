a3e7c775593a5c9bc4a74c081c713b0a26735648
Defelo <elodef42@gmail.com>
2022-04-15 11:02:11 +0200

    Improved serve.sh

--- serve.sh
+++ serve.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
+if [[ -z "$VIRTUAL_ENV" ]]; then
+    if ! [[ -e .venv ]]; then
+        echo Creating virtualenv
+        virtualenv .venv
+        install=1
+    fi
+
+    echo Activating virtualenv
+    . .venv/bin/activate
+    [[ "$install" = 1 ]] && pip install -r requirements.txt
+fi
+
+open() {
+    while ! curl -fso /dev/null http://127.0.0.1:8000; do
+        sleep 1
+    done
+    xdg-open http://localhost:8000/
+}
+
 tmux new -s docs -d mkdocs serve
-ff http://localhost:8000/
+open &
+tmux a -t docs
